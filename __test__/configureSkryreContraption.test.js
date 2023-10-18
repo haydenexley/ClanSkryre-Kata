f1632e00d174517450def8c04518d422a6aaeec2
haydenexley <haydenexley@outlook.com>
2023-10-18 13:47:48 +0100

    update configureSkryreContraption.js and relevant tests to complete kata

--- __test__/configureSkryreContraption.test.js
+++ __test__/configureSkryreContraption.test.js
@@ -1,5 +1,130 @@
 const configureSkryreContraption = require("../configureSkryreContraption");
 
-describe("ConfigureSkryreContratpion", () => {
-  test(() => {});
+describe("ConfigureSkryreContraption", () => {
+  test("returns an object with two empty arrays when given an empty array", () => {
+    const input = [];
+    const expectedOutput = { validComponents: [], invalidComponents: [] };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("returns an object of a component given", () => {
+    const input = [{ name: "Power Cell", type: "power source", rating: 5 }];
+    const expectedOutput = {
+      validComponents: [
+        { name: "Power Cell", type: "power source", rating: 5 },
+      ],
+      invalidComponents: [],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("returns an object of multiple components given", () => {
+    const input = [
+      { name: "Power Cell", type: "power source", rating: 5 },
+      { name: "Connector A", type: "connector", rating: 3 },
+      { name: "Circuit X", type: "circuit", rating: 2 },
+      { name: "Power Unit", type: "power source", rating: 4 },
+      { name: "Circuit Y", type: "circuit", rating: 3 },
+    ];
+    const expectedOutput = {
+      validComponents: [
+        { name: "Power Cell", type: "power source", rating: 5 },
+        { name: "Connector A", type: "connector", rating: 3 },
+        { name: "Circuit X", type: "circuit", rating: 2 },
+        { name: "Power Unit", type: "power source", rating: 4 },
+        { name: "Circuit Y", type: "circuit", rating: 3 },
+      ],
+      invalidComponents: [],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("checks components all have properties of name, type and rating ", () => {
+    const input = [
+      { name: "Power Cell", type: "power source", rating: 5 },
+      { name: "Connector A", type: "connector", rating: 3 },
+      { name: "Circuit X", type: "circuit", rating: 2 },
+      { name: "cheese", rating: 100 },
+      { type: "connector", rating: 1 },
+      { name: "Circuit Y", type: "circuit" },
+      { broken: "I wanna be a REAL component :(" },
+    ];
+    const expectedOutput = {
+      validComponents: [
+        { name: "Power Cell", type: "power source", rating: 5 },
+        { name: "Connector A", type: "connector", rating: 3 },
+        { name: "Circuit X", type: "circuit", rating: 2 },
+      ],
+      invalidComponents: [
+        { name: "cheese", rating: 100 },
+        { type: "connector", rating: 1 },
+        { name: "Circuit Y", type: "circuit" },
+        { broken: "I wanna be a REAL component :(" },
+      ],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("checks component types match one of the predefined types", () => {
+    const input = [
+      { name: "Power Cell", type: "power source", rating: 5 },
+      { name: "Connector Z", type: "not real part!", rating: 5 },
+    ];
+    const expectedOutput = {
+      validComponents: [
+        { name: "Power Cell", type: "power source", rating: 5 },
+      ],
+      invalidComponents: [
+        { name: "Connector Z", type: "not real part!", rating: 5 },
+      ],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("checks component ratings are positive integers", () => {
+    const input = [
+      { name: "Power Cell", type: "power source", rating: 5 },
+      { name: "Connector Z", type: "not real part!", rating: 5 },
+      { name: "Connector A", type: "connector", rating: -1 },
+    ];
+    const expectedOutput = {
+      validComponents: [
+        { name: "Power Cell", type: "power source", rating: 5 },
+      ],
+      invalidComponents: [
+        { name: "Connector Z", type: "not real part!", rating: 5 },
+        { name: "Connector A", type: "connector", rating: -1 },
+      ],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("checks contraptions have a power source", () => {
+    const input = [
+      { name: "Connector Z", type: "connector", rating: 5 },
+      { name: "Connector A", type: "connector", rating: 1 },
+    ];
+    const expectedOutput = {
+      validComponents: [],
+      invalidComponents: [
+        { name: "Connector Z", type: "connector", rating: 5 },
+        { name: "Connector A", type: "connector", rating: 1 },
+      ],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
+  test("checks contraptions don't have components with duplicate names", () => {
+    const input = [
+      { name: "Power Cell", type: "power source", rating: 5 },
+      { name: "Connector Z", type: "connector", rating: 5 },
+      { name: "Connector A", type: "connector", rating: 1 },
+      { name: "Connector Z", type: "connector", rating: 2 },
+      { name: "Circuit X", type: "circuit", rating: 3 },
+    ];
+    const expectedOutput = {
+      validComponents: [],
+      invalidComponents: [
+        { name: "Power Cell", type: "power source", rating: 5 },
+        { name: "Connector Z", type: "connector", rating: 5 },
+        { name: "Connector A", type: "connector", rating: 1 },
+        { name: "Connector Z", type: "connector", rating: 2 },
+        { name: "Circuit X", type: "circuit", rating: 3 },
+      ],
+    };
+    expect(configureSkryreContraption(input)).toEqual(expectedOutput);
+  });
 });
--- configureSkryreContraption.js
+++ configureSkryreContraption.js
@@ -1,3 +1,35 @@
-function configureSkryreContraption() {}
+function configureSkryreContraption(componentsArr) {
+  let validComponents = [];
+  let invalidComponents = [];
+  const acceptedTypes = ["power source", "connector", "circuit"];
+  const componentNames = [];
+  for (component of componentsArr) {
+    if (
+      component.hasOwnProperty("name") &&
+      component.hasOwnProperty("type") &&
+      component.hasOwnProperty("rating") &&
+      acceptedTypes.find((acceptedType) => component.type === acceptedType) &&
+      component.rating > 0
+    ) {
+      componentNames.push(component.name);
+      validComponents.push(component);
+    } else {
+      invalidComponents.push(component);
+    }
+  }
+  const hasPowerSource = validComponents.find(
+    (component) => component.type === "power source"
+  );
+  const noDuplicateNamesArr = new Set(componentNames);
+  if (
+    hasPowerSource === undefined ||
+    noDuplicateNamesArr.size !== componentNames.length
+  ) {
+    invalidComponents = invalidComponents.concat(validComponents);
+    validComponents.length = 0;
+  }
+  const sortedComponents = { validComponents, invalidComponents };
+  return sortedComponents;
+}
 
 module.exports = configureSkryreContraption;
